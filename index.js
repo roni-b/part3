1d1f57ddfd1b4e8e34827a44b685ae0552551d82
Roni Bärman <104189902+roni-b@users.noreply.github.com>
2023-04-17 15:39:09 +0300

    build

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('build'))
 
 morgan.token('body', function (req, res) {
   return JSON.stringify(req.body)
