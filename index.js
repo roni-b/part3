6e6ba0c8044567d85184b76c53e3c7dd542f032e
Roni Bärman <104189902+roni-b@users.noreply.github.com>
2023-04-19 21:01:51 +0300

    Update

--- index.js
+++ index.js
@@ -111,6 +111,7 @@ app.post('/api/persons', (req, res, next) => {
   newPerson.save().then(savedPerson => {
     res.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
@@ -120,7 +121,11 @@ app.put('/api/persons/:id', (req, res, next) => {
     number: body.number
   }
 
-  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    req.params.id,
+    person,
+    { new: true, runValidators: true, context: 'query' }
+    )
     .then(updatedPerson => {
       res.json(updatedPerson)
     })
@@ -138,6 +143,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
--- models/person.js
+++ models/person.js
@@ -15,8 +15,21 @@ mongoose.connect(url)
   })
 
   const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+      type: String,
+      minlength: 3,
+      required: true,
+      message: props => `400 ${props.value} is not a valid phone number!`
+    },
+    number: {
+      type: String,
+      validate: {
+        validator: function(v) {
+          return /\d{2,3}-\d{7}/.test(v);
+        },
+        message: props => `400 ${props.value} is not a valid phone number!`
+      }
+    }
   })
 
   personSchema.set('toJSON', {
